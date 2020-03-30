b29c45413337e08ec721f43f7c5fcd9f6f9fb372
solygambas <solygambas@gmail.com>
2020-03-30 13:51:06 +0200

    add webpack

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  mode: "development",
+  watch: true
+};
