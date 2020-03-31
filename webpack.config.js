12a3771f853792805cf1b9c2122b3719320b5d19
solygambas <solygambas@gmail.com>
2020-03-31 14:08:48 +0200

    add webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,16 @@ module.exports = {
     filename: "bundled.js",
     path: path.resolve(__dirname, "app")
   },
+  devServer: {
+    before: function(app, server) {
+      server._watch("./app/**/*.html");
+    },
+    contentBase: path.join(__dirname, "app"),
+    hot: true,
+    port: 3000,
+    host: "0.0.0.0"
+  },
   mode: "development",
-  watch: true,
   module: {
     rules: [
       {
