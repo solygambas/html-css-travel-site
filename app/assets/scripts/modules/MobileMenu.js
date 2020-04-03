88dd0c13b0dbe57cb35fc9d85c2830727e3c5a18
solygambas <solygambas@gmail.com>
2020-04-03 11:05:51 +0200

    add hamburger icon

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -13,6 +13,7 @@ class MobileMenu {
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible");
     this.siteHeader.classList.toggle("site-header--is-expanded");
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x");
   }
 }
 
