ea36e3113bc522e44b6537e1880be5cafb5a29ec
solygambas <solygambas@gmail.com>
2020-04-03 10:29:57 +0200

    add mobile menu styling

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -2,6 +2,7 @@ class MobileMenu {
   constructor() {
     this.menuIcon = document.querySelector(".site-header__menu-icon");
     this.menuContent = document.querySelector(".site-header__menu-content");
+    this.siteHeader = document.querySelector(".site-header");
     this.events();
   }
 
@@ -11,6 +12,7 @@ class MobileMenu {
 
   toggleTheMenu() {
     this.menuContent.classList.toggle("site-header__menu-content--is-visible");
+    this.siteHeader.classList.toggle("site-header--is-expanded");
   }
 }
 
