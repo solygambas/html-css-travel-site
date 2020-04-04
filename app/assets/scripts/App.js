634075559080fc2dbc7137d33975e4937cb1d42a
solygambas <solygambas@gmail.com>
2020-04-04 10:52:52 +0200

    add modal JS

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,7 +2,9 @@ import "../styles/styles.css";
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
 import StickyHeader from "./modules/StickyHeader";
+import Modal from "./modules/Modal";
 
+new Modal();
 let mobileMenu = new MobileMenu();
 let stickyHeader = new StickyHeader();
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
