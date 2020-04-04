171b8c6be68bfb255570fc69623d0df9cd9f4541
solygambas <solygambas@gmail.com>
2020-04-04 08:15:33 +0200

    add active link

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -16,14 +16,14 @@ class RevealOnScroll {
     window.addEventListener(
       "resize",
       debounce(() => {
-        console.log("resize just ran");
+        // console.log("resize just ran");
         this.browserHeight = window.innerHeight;
       }, 333)
     );
   }
 
   calcCaller() {
-    console.log("scroll function ran");
+    // console.log("scroll function ran");
     this.itemsToReveal.forEach(el => {
       if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el);
@@ -33,7 +33,7 @@ class RevealOnScroll {
 
   calculateIfScrolledTo(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop) {
-      console.log("element was calculated");
+      // console.log("element was calculated");
       let scrollPercent =
         (el.getBoundingClientRect().y / this.browserHeight) * 100;
       if (scrollPercent < this.thresholdPercent) {
