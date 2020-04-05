6a3e15d4afd2cbf05854f325431c7c38bbbf36b0
solygambas <solygambas@gmail.com>
2020-04-05 10:51:05 +0200

    add checking logic

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,25 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: "Welcome to the super secret area",
-  });
+  const secretContent = `
+    <h3>Welcome to the Secret Area</h3>
+    <p>Here we can tell you that the sky is <strong>blue</strong>, and two plus two equals four.</p>
+    `;
+
+  let body;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password == "javascript") {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
